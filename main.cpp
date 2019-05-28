66a10db3cd668ad4616c0ae66463c95de29eefae
ZobovPA <48247852+ZobovPA@users.noreply.github.com>
2019-05-28 12:44:19 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter A and B: ";
+    double a, b;
+    cin >> a >> b;
+    cout << "A + B = " << a + b << '\n'
+         << "A - B = " << a - b << '\n'
+         << "A * B = " << a * b << '\n'
+         << "A / B = " << a / b << '\n'
+         << "max = " << max( a, b ) << '\n'
+         << "min = " << min( a, b ) << '\n';
+    return 0;
+}
